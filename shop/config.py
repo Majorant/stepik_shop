2160205e96e83bdb574396605ab6d5fa7e0b4063
majorant <majorant@yandex.ru>
2020-12-12 20:33:18 +0300

    update Procfile and config, prepare to geroku upload

--- shop/config.py
+++ shop/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY= os.environ.get("CSRF_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
